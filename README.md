17a085070ba9b4f7456404fb4fc56e4319ef0d92
drowkid-1 <pxne031112hmcrgda1@bggem.mx>
2023-08-31 02:36:26 -0500

    Powered by DrowKid

---
+++ README.md
@@ -0,0 +1,11 @@
+# NgrokTH
+Instalación Correcta De Ngrok En Termux
+#
+# INSTALACIÓN
+#
+chmod 711 ngrok.sh 			./ngrok.sh
+#
+Escriba su authtoken de Ngrok y pulse Enter
+#
+# Created by: DrowKid
+
---
+++ ngrok.sh
@@ -0,0 +1,56 @@
+#!/bin/bash#
+# VARIABLES
+#
+PWD=$(pwd)
+source ${PWD}/.colores
+#
+# FUNCIONES
+#
+function Install_Ngrok {
+	sleep 0.5
+	clear
+echo -e "${verde}
+ ▄▄▄   ▄▄     ▄▄▄▄   ▄▄▄▄▄▄      ▄▄▄▄    ▄▄   ▄▄▄
+ ███   ██   ██▀▀▀▀█  ██▀▀▀▀██   ██▀▀██   ██  ██▀
+ ██▀█  ██  ██        ██    ██  ██    ██  ██▄██
+ ██ ██ ██  ██  ▄▄▄▄  ███████   ██    ██  █████
+ ██  █▄██  ██  ▀▀██  ██  ▀██▄  ██    ██  ██  ██▄
+ ██   ███   ██▄▄▄██  ██    ██   ██▄▄██   ██   ██▄
+ ▀▀   ▀▀▀     ▀▀▀▀   ▀▀    ▀▀▀   ▀▀▀▀    ▀▀    ▀▀
+"${blanco}
+}
+function token_ngrok {
+	Install_Ngrok
+	unzip ngrok-stable-linux-arm.zip
+	chmod 777 ngrok
+	cp ngrok ${PREFIX}/bin
+echo -e -n "${verde}
+┌═════════════════┐
+█ ${blanco}NGROK AUTHTOKEN ${verde}█
+└═════════════════┘
+┃
+└═>>> "${blanco}
+read -r auth_token_ngrok
+${auth_token_ngrok}
+rm ngrok
+echo -e "${verde}
+┌═══════════════════════════════┐
+█ ${blanco}NGROK INSTALADO CORRECTAMENTE ${verde}█
+└═══════════════════════════════┘
+"${blanco}
+}
+#
+# CÓDIGO
+#
+if [ -x ${PREFIX}/bin/ngrok ]
+then
+Install_Ngrok
+echo -e "${verde}
+┌═════════════════════════┐
+█ ${blanco}NGROK YA ESTÁ INSTALADO ${verde}█
+└═════════════════════════┘
+Powered by DrowKid
+"${blanco}
+else
+token_ngrok
+fi
